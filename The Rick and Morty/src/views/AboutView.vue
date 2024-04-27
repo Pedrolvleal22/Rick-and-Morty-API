76cbb49cdda17419353bba45af58c921fbaef709
Pedrolvleal22 <128000355+Pedrolvleal22@users.noreply.github.com>
2024-04-27 16:05:47 -0300

    Add files via upload

---
+++ The Rick and Morty/src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    Aluno: Pedro Lucas Vasconcelos Leal <br>
+    RA: 1968204 <br>
+    Curso: Análise e Desenvolvimento de Sistemas<br>
+    Turma: 3C<br>
+    Unimar<br>
+  </div>
+</template>
